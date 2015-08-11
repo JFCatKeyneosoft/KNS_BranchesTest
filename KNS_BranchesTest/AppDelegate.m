0368de1809d844a9847fcc3660c2317ba9c41ef6
JFCatKeyneosoft <jfcontart@keyneosoft.fr>
2015-08-11 10:42:12 +0200

    I add dev

--- KNS_BranchesTest/AppDelegate.m
+++ KNS_BranchesTest/AppDelegate.m
@@ -14,6 +14,7 @@
 
 @implementation AppDelegate
 
+// ADD DEV
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
